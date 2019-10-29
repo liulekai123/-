03805257225454cbec812208e29fc0eb1d9c3a42
oneoiece11 <2211968245@qq.com>
2019-10-29 09:11:13 +0800

    78 4-6

--- 1.c
+++ 1.c
@@ -1,24 +1,40 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main(void)
 {
-    int x,y,z,t;
-    scanf("%d%d%d",&x,&y,&z);
-    if(x>y)
+    int count = 0, mynumber, yournumber, n;
+    srand(time(0));
+    mynumber = rand() % 100 + 1;
+    printf("n=");
+    scanf("%d", &n);
+    while (count <= n)
     {
-        t=x;x=y;y=t;    
+        printf("yournumber=");
+        scanf("%d", &yournumber);
+        if (yournumber < 0)
+            break;
+        count++;
+        if (yournumber > mynumber)
+        {
+            printf("too big\n");
+        }
+        else if (yournumber < mynumber)
+        {
+            printf("too small\n");
+        }
+        else if (yournumber = mynumber)
+            break;
     }
-    if(x>z)
-    {
-        t=x;x=z;z=t;
-
-    }
-    if(y>z)
-    {
-        t=y;y=z;z=t;
-    }
-    printf("%d<%d<%d",x,y,z);
-
+    if (count == 0)
+        printf("game over");
+    if (count == 1)
+        printf("bingo");
+    if (count == 2 || count == 3)
+        printf("lucky you");
+    if (count > 3 && count < n)
+        printf("good gues");
+    if (count > n)
+        printf("game over");
     return 0;
 }
-
