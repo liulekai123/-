a27aa257d1cff27b6b9131b893191f525c7e1d62
Your Name <you@example.com>
2019-11-11 23:08:52 +0800

    作业11.12

--- 10.c
+++ 10.c
@@ -1,18 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(void)
 {
-    int n,scores;
+    int n, scores,A=0,B=0,C=0,D=0,E=0;
     printf("n=");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("scores=");
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&scores);
-        if(scores>=90)printf("A");
-        else if(scores>=80&&scores<90)printf("B");
-        else if(scores>=70&&scores<80)printf("C");
-        else if(scores>=60&&scores<70)printf("D");
-        else if(scores<60)printf("E");
+        scanf("%d", &scores);
+        if (scores >= 90)
+            A++;
+        else if (scores >= 80 && scores < 90)
+            B++;
+        else if (scores >= 70 && scores < 80)
+            C++;
+        else if (scores >= 60 && scores < 70)
+            D++;
+        else if (scores < 60)
+            E++;
     }
+    printf("A=%d\nB=%d\nC=%d\nD=%d\nE=%d\n",A,B,C,D,E);
     return 0;
 }
