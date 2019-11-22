2125de4e262b1f82824febd1870ccf4cc1368fdd
Your Name <you@example.com>
2019-11-22 16:56:59 +0800

    作业11.25

--- 6.c
+++ 6.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(void)
 {
-    double dianliang,dianfei;
-    printf("dianliang=");
-    scanf("%lf",&dianliang);
-    if(dianliang<=50) dianfei=0.53*dianliang;
-    else dianfei=0.53*50+0.58*(dianliang-50);
-    printf("dianfei=%lf",dianfei);
+    int male,female,small;
+    for(male=0;male<=20;male++)
+    {
+        for(female=0;female<=33;female++)
+        {
+            small=100-male-female;
+            if(5*male+3*female+small/3==100) printf("male=%d,female=%d,small=%d\n",male,female,small);
+        }
+    }
     return 0;
 }
