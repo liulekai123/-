0fd0f77329714023f64873faf8087a16cc1e084e
Your Name <you@example.com>
2019-11-25 22:26:14 +0800

    224面 9-2

--- 1.c
+++ 1.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
-#include<math.h>
+struct brithday {
+    int year;
+    int mouth;
+    int day;
+};
+struct stdudent {
+    int num;
+    char name [10];
+    int computer,english,math;
+    double average;
+    struct brithday a;
+};
 int main(void)
 {
-    int min=1,sum=0,n,i,item;
+    int i,n;
+    struct stdudent s1,max;
     printf("n=");
     scanf("%d",&n);
-    for(i=1;i<n;i++)min=min*10;
-    int e=min*10;
-    for(i=min;i<e;i++)
+    printf("input the stdent's number,name,course scores and brithday:\n");
+    for(i=1;i<=n;i++)
     {
-        int b,c;
-        b=c=i;
-        min=e/10;
-        sum=0;
-        while(min>0)
-        {
-            item=c/min;
-            sum=sum+pow(item,n);
-            c=c%min;
-            min=min/10;
-        }
-        if(sum==b)printf("%d\n",b);
+        printf("no.%d:",i);
+        scanf("%d%s%d%d%d%d%d%d",&s1.num,s1.name,&s1.math,&s1.english,&s1.computer,&s1.a.year,&s1.a.mouth,&s1.a.day);
+        s1.average=(s1.math+s1.english+s1.computer)/3.0;
+        if(i==1) max=s1;
+        if(max.average<s1.average) max=s1;
     }
-    return 0;
-} 
+     printf("num:%d,name:%s,average:%.2lf,birthday:%d-%d-%d",max.num,max.name,max.average,max.a.year,max.a.mouth,max.a.day);
+}
